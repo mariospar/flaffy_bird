afa0e94840f1f3a848fdac6679dc251bb39dbb1c
Marios Paraskevas <mariosparaskevas00@gmail.com>
2023-01-25 05:14:40 +0200

    Fixed pipes, added game over, flap and collision sounds

--- main.lua
+++ main.lua
@@ -1,111 +1,107 @@
--- Initialize game variables
 local bird = {}
 bird.x = 100
 bird.y = 300
-bird.dy = 0
+bird.speed = 0
 local pipes = {}
+local score = 0
 local gameover = false
-local last_pipe = 0
-local pipe_interval = 1
-
-score = 0
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
-
--- Load assets
-local birdImage = love.graphics.newImage("assets/sprites/bird.png")
-local pipeImage = love.graphics.newImage("assets/sprites/pipe.png")
+local jumpSound
+local collisionSound
+local gameOverSound
+local gameOverSoundPlayed = false
+local pointSound
 local background = love.graphics.newImage("assets/images/expanded.png")
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-    love.window.setTitle('Falpy Bird')
-    table.insert(pipes, {
-        x = 300,
-        y = 0
-    })
+    love.window.setTitle('Flaffy Bird')
+    bird.image = love.graphics.newImage("assets/sprites/bird.png")
+    pipes.top = love.graphics.newImage("assets/sprites/pipeTop.png")
+    pipes.bottom = love.graphics.newImage("assets/sprites/pipeBottom.png")
+    jumpSound = love.audio.newSource("assets/sounds/bird-flap.wav", "static")
+    collisionSound = love.audio.newSource("assets/sounds/bird-hurt.wav", "static")
+    gameOverSound = love.audio.newSource("assets/sounds/game-over.mp3", "static")
+    pointSound = love.audio.newSource("assets/sounds/point.mp3", "static")
+    for i = 1, 3 do
+        pipes[i] = {}
+        pipes[i].x = love.graphics.getWidth() + (i - 1) * 200
+        pipes[i].y = math.random(200, 300)
+    end
+    gameover = false
+    score = 0
 end
 
-local function check_collision(x1, y1, w1, h1, x2, y2, w2, h2)
-    return x1 < x2 + w2 and x2 < x1 + w1 and y1 < y2 + h2 and y2 < y1 + h1
+function CheckCollision(ax1, ay1, aw, ah, bx1, by1, bw, bh)
+    local ax2, ay2, bx2, by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
+    return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
 end
 
--- Game loop
 function love.update(dt)
-    local current_time = love.timer.getTime()
-    if current_time - last_pipe > pipe_interval then
-        last_pipe = current_time
-        table.insert(pipes, {
-            x = love.graphics.getWidth(),
-            y = math.random(0, love.graphics.getHeight() - pipeImage:getHeight())
-        })
-    end
-
-    -- Update bird position
-    bird.dy = bird.dy + 9.8 * dt
-    bird.y = bird.y + bird.dy
-
-    -- Handle user input
-    if love.keyboard.isDown("space") then
-        bird.dy = -4
-    end
-
-    -- update pipes
-    for i = 1, #pipes do
-        pipes[i].x = pipes[i].x - 100 * dt
-    end
-
-    -- Check for collisions
-    for i = 1, #pipes do
-        if check_collision(bird.x, bird.y, birdImage:getWidth(), birdImage:getHeight(), pipes[i].x, pipes[i].y,
-            pipeImage:getWidth(), pipeImage:getHeight()) then
-            gameover = true
-        end
-
-        if bird.x > pipes[i].x + pipeImage:getWidth() then
-            score = score + 1
-        end
-    end
     if gameover then
-        if love.keyboard.isDown("r") then
-            -- reset game
-            gameover = false
-            score = 0
-            bird.x = 100
-            bird.y = 300
-            bird.dy = 0
-            for i = 1, 3 do
-                pipes[i] = {}
-                pipes[i].x = 300 + (i - 1) * 200
-                pipes[i].y = math.random(200, 400)
-            end
+        if gameOverSoundPlayed == true then
+            return
+        end
+        love.audio.play(gameOverSound)
+        gameOverSoundPlayed = true
+        return
+    end
+
+    if love.keyboard.isDown("space") then
+        bird.speed = -220
+    end
+
+    bird.y = bird.y + bird.speed * dt
+    bird.speed = bird.speed + 800 * dt
+
+    if bird.y < 0 then
+        bird.y = 0
+    elseif bird.y + bird.image:getHeight() > love.graphics.getHeight() then
+        bird.y = love.graphics.getHeight() - bird.image:getHeight()
+    end
+
+    for i = 1, #pipes do
+        pipes[i].x = pipes[i].x - 200 * dt
+        if pipes[i].x < -50 then
+            pipes[i].x = love.graphics.getWidth()
+            pipes[i].y = math.random(100, 400)
+        end
+
+        if CheckCollision(bird.x, bird.y, bird.image:getWidth(), bird.image:getHeight(), pipes[i].x, pipes[i].y,
+            pipes.top:getWidth(), pipes.top:getHeight()) or
+            CheckCollision(bird.x, bird.y, bird.image:getWidth(), bird.image:getHeight(), pipes[i].x,
+                pipes[i].y - pipes.top:getHeight() - 100, pipes.bottom:getWidth(), pipes.bottom:getHeight()) then
+            love.audio.play(collisionSound)
+            gameover = true
         end
     end
 end
 
 function love.keypressed(key)
+    if key == "r" and gameover then
+        love.load()
+    end
+
+    if key == "space" and not gameover then
+        love.audio.play(jumpSound)
+    end
+
     if key == 'escape' then
         love.event.quit()
     end
 end
 
 function love.draw()
-    -- Draw bird
     love.graphics.draw(background, 0, 0, 0, love.graphics.getWidth() / background:getWidth(),
         love.graphics.getHeight() / background:getHeight())
-
-    love.graphics.draw(birdImage, bird.x, bird.y)
-
-    -- Draw pipes
-    for i, pipe in ipairs(pipes) do
-        love.graphics.draw(pipeImage, pipe.x, pipe.y)
+    love.graphics.draw(bird.image, bird.x, bird.y)
+    for i = 1, #pipes do
+        love.graphics.draw(pipes.bottom, pipes[i].x, pipes[i].y)
+        love.graphics.draw(pipes.top, pipes[i].x, pipes[i].y - pipes.top:getHeight() - 100)
     end
-
-    if gameover then
-        love.graphics.print("Game Over", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 0, 2, 2)
-        love.graphics.print("Press R to restart", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2 + 20, 0,
-            2, 2)
-    end
-
     love.graphics.print("Score: " .. score, 10, 10)
+    if gameover then
+        love.graphics.print("Game Over", love.graphics.getWidth() / 2 - 50, love.graphics.getHeight() / 2 - 10)
+        love.graphics.print("Press 'R' to restart", love.graphics.getWidth() / 2 - 75,
+            love.graphics.getHeight() / 2 + 20)
+    end
 end
